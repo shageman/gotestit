3a93c561aca9d53d42da7b9e7d368cd43bbe5396
Stephan Hagemann <stephan@pivotallabs.com>
2013-07-05 10:58:29 -0600

    loggregator client sample code and testify tests

---
+++ src/gotestit/loggregatorclient_testify/loggregatorclient_test.go
@@ -0,0 +1,56 @@
+package loggregatorclient_testify
+
+import (
+	l "gotestit/loggregatorclient"
+	"github.com/cloudfoundry/gosteno"
+	"github.com/stretchr/testify/assert"
+	"net"
+	"testing"
+)
+
+func TestSend(t *testing.T) {
+	bufferSize := 4096
+	expectedOutput := []byte("Important Testmessage")
+	loggregatorClient := l.NewLoggregatorClient("localhost:9876", gosteno.NewLogger("TestLogger"), bufferSize)
+
+	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:9876")
+	assert.NoError(t, err)
+
+	udpListener, err := net.ListenUDP("udp", udpAddr)
+	defer udpListener.Close()
+	assert.NoError(t, err)
+
+	loggregatorClient.Send(expectedOutput)
+
+	buffer := make([]byte, bufferSize)
+	readCount, _, err := udpListener.ReadFromUDP(buffer)
+	assert.NoError(t, err)
+
+	received := string(buffer[:readCount])
+	assert.Equal(t, string(expectedOutput), received)
+}
+
+func TestDontSendEmptyData(t *testing.T) {
+	bufferSize := 4096
+	firstMessage := []byte("")
+	secondMessage := []byte("hi")
+	loggregatorClient := l.NewLoggregatorClient("localhost:9876", gosteno.NewLogger("TestLogger"), bufferSize)
+
+	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:9876")
+	assert.NoError(t, err)
+
+	udpListener, err := net.ListenUDP("udp", udpAddr)
+	defer udpListener.Close()
+	assert.NoError(t, err)
+
+	loggregatorClient.Send(firstMessage)
+	loggregatorClient.Send(secondMessage)
+
+	buffer := make([]byte, bufferSize)
+	readCount, _, err := udpListener.ReadFromUDP(buffer)
+
+	assert.NoError(t, err)
+
+	received := string(buffer[:readCount])
+	assert.Equal(t, string(secondMessage), received)
+}
